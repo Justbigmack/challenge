e2479d45ecbf25d39f60870e2bb6e51050c8dc72
Andrei Karas <38630949+Justbigmack@users.noreply.github.com>
2022-01-26 17:25:02 +0300

    Fixed a bug with full calendar css imports

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withTM = require("next-transpile-modules")([
+  "@fullcalendar/common",
+  "@babel/preset-react",
+  "@fullcalendar/common",
+  "@fullcalendar/daygrid",
+  "@fullcalendar/interaction",
+  "@fullcalendar/react",
+  "@fullcalendar/timegrid",
+]);
+const nextConfig = withTM({
   reactStrictMode: true,
-}
+})
 
 module.exports = nextConfig
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,16 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+import Header from '../components/Header';
+import '../styles/globals.css';
+import 'react-datepicker/dist/react-datepicker.css';
+import '@fullcalendar/common/main.css';
+import '@fullcalendar/daygrid/main.css';
+import '@fullcalendar/timegrid/main.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ChakraProvider>
+      <Header />
       <Component {...pageProps} className="h-full" />
     </ChakraProvider>
   );
